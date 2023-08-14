96927a231ff131d627e41941dbd823c3098f2cdb
OvaisKhanday <ovaiskhanday927@gmail.com>
2023-08-14 11:52:09 +0530

    FIRST try to upload to railway.app

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import https from "https";
+import "dotenv/config";
+import home from "./routes/home.js";
+
+const app = express();
+
+// support JSON-encoded and URL-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT;
+
+// https.createServer({...}, app).listen(port);
+
+// listen to the server
+app.listen(port, (req, res) => {
+  app.use("/", home);
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+// app.use("/login", login);
+
+// app.use("/driver", driver);
+// var http = require('http') , https = require('https') , express = require('express') , app = express();
+
+// http.createServer(app).listen(80); https.createServer({ ... }, app).listen(443);
+
+// app.use("/parent", parent);
